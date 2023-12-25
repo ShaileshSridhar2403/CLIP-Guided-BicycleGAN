53b1715e0174bdcd70b4a3ada47b5473048832b2
ShaileshSridhar2403 <shailesh.sridhar@gmail.com>
2023-12-24 23:40:01 -0500

    updated train.py and model.py

--- src/models.py
+++ src/models.py
@@ -88,113 +88,79 @@ def double_convolution(inchannels, outchannels, normalize=True):
 
 
 
+# This is similar to Cyclegan generator except we incorporate latent dimension
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features),
+            nn.ReLU(inplace=True),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_features, in_features, 3),
+            nn.InstanceNorm2d(in_features),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+
 class Generator(nn.Module):
-    """ The generator used in both cVAE-GAN and cLR-GAN, which transform A to B
-        
-        Args in constructor: 
-            latent_dim: latent dimension for z 
-            image_shape: (channel, h, w), you may need this to specify the output dimension (optional)
-        
-        Args in forward function: 
-            x: image input (from domain A)
-            z: latent vector (encoded B)
-
-        Returns: 
-            fake_B: generated image in domain B
-    """
-    def __init__(self, latent_dim, img_shape):
+    def __init__(self, latent_dim, input_shape, num_residual_blocks, device='cuda'):
         super(Generator, self).__init__()
-        channels, self.h, self.w = img_shape
-        
-        # Define UNET architecture
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride = 2)
-        self.fc = nn.Linear(latent_dim,self.h * self.w) #converts z from 1d to 2d
 
-        #downsampling part
-        self.downconv1 = double_convolution(channels + 1,64,normalize=False) #channels +1 because extra depth of z is also added
-        self.downconv2 = double_convolution(64,128,normalize=True)
-        self.downconv3 = double_convolution(128,256,normalize=True)
-        self.downconv4 = double_convolution(256,512,normalize=True)
-        self.downconv5 = double_convolution(512,1024,normalize=False)
-        
+        self.device = device
 
-        #upsampling part
-        #in channels below -> we also incorporate z feature
-        self.transposeconv1 = nn.ConvTranspose2d(in_channels=1024,out_channels = 512,kernel_size=(2,2),stride = 2)
-        #double convolutions again
-        self.upconv1 = double_convolution(1024,512,normalize=True) #1024 because after skip connections -> 512+512
-        self.transposeconv2 = nn.ConvTranspose2d(in_channels=512,out_channels = 256,kernel_size=(2,2),stride = 2)
-        self.upconv2 = double_convolution(512,256,normalize=True)
-        self.transposeconv3 = nn.ConvTranspose2d(in_channels=256,out_channels = 128,kernel_size=(2,2),stride = 2)
-        self.upconv3 = double_convolution(256,128,normalize=True)
-        self.transposeconv4 = nn.ConvTranspose2d(in_channels=128,out_channels = 64,kernel_size=(2,2),stride = 2)
-        self.upconv4 = double_convolution(128,64,normalize=True)
-        
-        #1*1 convolution
-        self.final_conv = nn.Conv2d(in_channels = 64, out_channels = channels,kernel_size = 1)
+        channels = input_shape[0]
 
-    def forward(self, x, z):
-        # print("x shape initially",x.shape)  #torch.Size([4, 3, 256, 256])
-        # print("z shape initially",z.shape)  #torch.Size([4, 8])
-        
-        # convert 2d z to 4d z so that it can be concatenated with image
-        z = self.fc(z).view(z.size(0), 1, self.h, self.w)
-        # print(z.shape)                      #torch.Size([4, 1, 256, 256])
-        
-        #now this z is concatenated with x
-        x1 = self.downconv1(torch.cat([x, z], dim=1))
-        # print(x1.shape)                     #torch.Size([4, 64, 256, 256])
-        x2 = self.maxpool(x1)
-        # print(x2.shape)                     #torch.Size([4, 64, 128, 128])
-        x3 = self.downconv2(x2)
-        # print(x3.shape)                     #torch.Size([4, 128, 128, 128])
-        x4 = self.maxpool(x3)
-        # print(x4.shape)                     #torch.Size([4, 128, 64, 64])
-        x5 = self.downconv3(x4)
-        # print(x5.shape)                     #torch.Size([4, 256, 64, 64])
-        x6 = self.maxpool(x5)
-        # print(x6.shape)                     #torch.Size([4, 256, 32, 32])
-        x7 = self.downconv4(x6)
-        # print(x7.shape)                     #torch.Size([4, 512, 32, 32])
-        x8 = self.maxpool(x7)
-        # print(x8.shape)                     #torch.Size([4, 512, 16, 16])
-        x9 = self.downconv5(x8)
-        # print(x9.shape)                     #torch.Size([4, 1024, 16, 16])
-        
-              
-        #upsampling
-        x = self.transposeconv1(x9)
-        # print(x.shape)                      #torch.Size([4, 512, 32, 32])
-        #we need to crop x7 to fit x's size before we can perform skip connections concatenation
-        y = crop_image(x7,x)
-        # print(y.shape)                      #torch.Size([4, 512, 32, 32]) 
-        x = self.upconv1(torch.cat([x,y],1))  #This step involves concatenating 512 channels + 512 channels but i also do upconv so resultant will have 512 channels again as in unet paper
-        # print(x.shape)                      #torch.Size([4, 512, 32, 32])
-        
-        x = self.transposeconv2(x)   
-        # print(x.shape)                      #torch.Size([4, 256, 64, 64])
-        #but before that we need to crop x5 to fit x's size
-        y = crop_image(x5,x)
-        x = self.upconv2(torch.cat([x,y],1))
-        # print(x.shape)                      #torch.Size([4, 256, 64, 64])
-        
-        x = self.transposeconv3(x) 
-        # print(x.shape)                      #    torch.Size([4, 128, 128, 128])
-        #but before that we need to crop x3 to fit x's size
-        y = crop_image(x3,x)
-        x = self.upconv3(torch.cat([x,y],1))
-        # print(x.shape)                      #torch.Size([4, 128, 128, 128])
-        
-        x = self.transposeconv4(x)  
-        # print(x.shape)                      #torch.Size([4, 64, 256, 256]) 
-        #but before that we need to crop x1 to fit x's size
-        y = crop_image(x1,x)
-        x = self.upconv4(torch.cat([x,y],1))
-        # print(x.shape)                      #torch.Size([4, 64, 256, 256])
-        
-        #output
-        x = self.final_conv(x)              #torch.Size([4, 3, 256, 256])
-        return x
+        # Initial convolution block
+        out_features = 64
+        model = [
+            nn.ReflectionPad2d(channels),
+            nn.Conv2d(channels + latent_dim, out_features, 7),
+            # nn.InstanceNorm2d(out_features),
+            #nn.InstanceNorm2d(out_features),
+            nn.ReLU(inplace=True),
+        ]
+        in_features = out_features
+
+        # Downsampling
+        for _ in range(2):
+            out_features *= 2
+            model += [
+                nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+                nn.InstanceNorm2d(out_features),
+                nn.ReLU(inplace=True),
+            ]
+            in_features = out_features
+
+        # Residual blocks
+        for _ in range(num_residual_blocks):
+            model += [ResidualBlock(out_features)]
+
+        # Upsampling
+        for _ in range(2):
+            out_features //= 2
+            model += [
+                nn.Upsample(scale_factor=2),
+                nn.Conv2d(in_features, out_features, 3, stride=1, padding=1),
+                nn.InstanceNorm2d(out_features),
+                nn.ReLU(inplace=True),
+            ]
+            in_features = out_features
+
+        # Output layer
+        model += [nn.ReflectionPad2d(channels), nn.Conv2d(out_features, channels, 7), nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x: torch.Tensor, z: torch.Tensor):
+        z_img = z.view(z.size(0), z.size(1), 1, 1).expand(
+            z.size(0), z.size(1), x.size(2), x.size(3))
+        out = torch.cat([x, z_img], dim=1)
+        return self.model(out)
 
 
 ##############################
@@ -238,7 +204,7 @@ class Discriminator(nn.Module):
           x = self.leaky_relu(self.batch_norm2d_1(self.conv2(x)))
           x = self.leaky_relu(self.batch_norm2d_2(self.conv3(x)))
           x = self.leaky_relu(self.conv4(x))
-          
+
           # Patch classification
           patch_scores = self.patch_classifier(x)
 
--- src/train.py
+++ src/train.py
@@ -84,11 +84,70 @@ loss_D_loss_cVAE_history = []
 loss_D_loss_cLRGAN_history = []
 loss_clrgan_l1_history = []
 
+def plot_loss(total_loss_history,loss_cvaegan_l1_history,
+             loss_KLD_history,loss_D_loss_cVAE_history,
+             loss_D_loss_cLRGAN_history,
+             loss_clrgan_l1_history,loss_vae_gan_loss_history,loss_LR_GAN_loss_history):
+    for loss_ind in range(len([total_loss_history,loss_cvaegan_l1_history,
+             loss_KLD_history,loss_D_loss_cVAE_history,
+             loss_D_loss_cLRGAN_history,
+             loss_clrgan_l1_history,
+             loss_vae_gan_loss_history,
+             loss_LR_GAN_loss_history])):
 
+        loss = [total_loss_history,loss_cvaegan_l1_history,
+                loss_KLD_history,loss_D_loss_cVAE_history,
+                loss_D_loss_cLRGAN_history,
+                loss_clrgan_l1_history,loss_vae_gan_loss_history,loss_LR_GAN_loss_history][loss_ind]
+        #print(loss)
+
+        key = ["total_loss_history","loss_cvaegan_l1_history",
+                "loss_KLD_history","loss_D_loss_cVAE_history",
+                "loss_D_loss_cLRGAN_history",
+                "loss_clrgan_l1_history","loss_vae_gan_loss_history","loss_LR_GAN_loss_history"]
+
+        plt.plot(loss,label = key[loss_ind])
+
+        plt.legend()
+        plt.show()
+
+
+def plot_sketch_to_multimodal(generator,sketch,z_dim,n=5):
+    sketches = sketch.repeat(n,1,1,1)
+    z_random = torch.randn(n,z_dim).to(gpu_id)
+    #print(z_random)
+    out = generator(sketches,z_random)
+
+    fig, axs = plt.subplots(1,6, figsize = (15,90
+                                            ))
+    axs[0].imshow(sketch.detach().cpu().numpy().transpose(1,2,0))
+    axs[0].set_title('Sketch')
+    for i in range(1,n+1):
+        img = denorm(out[i-1].squeeze()).cpu().data.numpy().astype(np.uint8)
+        axs[i].imshow(img.transpose(1,2,0))  #real sketch
+        axs[i].set_title(f'Colored image {i}')
+
+    plt.show()
+
+
+def calculate_D_loss(fake_B, D, real_B):
+
+    # forward real_B images into the discriminator
+    real_D_scores = D(real_B)
+    # compute loss between Valid_label and discriminator output on real_B images
+    mse_loss_real = mse_loss(real_D_scores, torch.ones_like(real_D_scores))
+
+    # Compute loss between Fake_label and discriminator output on fake images
+    fake_D_scores = D(fake_B.detach())
+    mse_loss_fake = mse_loss(fake_D_scores, torch.zeros_like(fake_D_scores))
+    # sum real_B loss and fake loss as the loss_D
+    loss_discriminator = mse_loss_real + mse_loss_fake
+
+    return loss_discriminator
 
 
 # Training
-def train(argpath = None):
+def train(argpath = None, ckt_path = None):
     if argpath is not None:
         img_dir = argpath
         dataset = Edge2Shoe(img_dir)
@@ -96,110 +155,176 @@ def train(argpath = None):
 
     total_steps = len(loader)*num_epochs; step = 0
     report_feq = 1000
+    save_feq = 6229
+    start_epoch = 0
+
+    # Define generator, encoder and discriminators object
+    generator = Generator(latent_dim, img_shape, n_residual_blocks, device=gpu_id).to(gpu_id)
+    encoder = Encoder(latent_dim).to(gpu_id)
+    D_VAE = Discriminator().to(gpu_id)
+    D_LR = Discriminator().to(gpu_id)
+
+    # Define optimizers for networks
+    optimizer_E = torch.optim.Adam(encoder.parameters(), lr=lr_rate, betas=betas)
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=lr_rate, betas=betas)
+    optimizer_D_VAE = torch.optim.Adam(D_VAE.parameters(), lr=lr_rate, betas=betas)
+    optimizer_D_LR = torch.optim.Adam(D_LR.parameters(), lr=lr_rate, betas=betas)
+
+    #Initialzing empty loss histories. If checkpoint present, they get replaced below
     total_loss_history = []
     loss_cvaegan_l1_history = []
     loss_KLD_history = []
     loss_D_loss_cVAE_history = []
     loss_D_loss_cLRGAN_history = []
     loss_clrgan_l1_history = []
+    loss_vae_gan_loss_history = []
+    loss_LR_GAN_loss_history = []
 
+    #Load checkpoint if path is given
+    if ckt_path:
+        if os.path.isfile(ckt_path):
+            print(f"Loading checkpoint '{ckt_path}'")
+            ckt = torch.load(ckt_path)
+            #load losses
+            total_loss_history = ckt['loss_total']
+            #print("checkpointed total loss history : ",total_loss_history)
+            loss_cvaegan_l1_history = ckt['cvaegan_l1']
+            loss_KLD_history = ckt['KLD']
+            loss_D_loss_cVAE_history = ckt['D_loss_cVAE']
+            loss_D_loss_cLRGAN_history =ckt['D_loss_cLRGAN']
+            loss_clrgan_l1_history = ckt['clrgan_l1']
+            loss_vae_gan_loss_history = ckt['VAE_GAN_loss']
+            loss_LR_GAN_loss_history = ckt['LR_GAN_loss']
+            #load gen,disc,encoder objects
+            generator.load_state_dict(ckt['generator'])
+            encoder.load_state_dict(ckt['encoder'])
+            D_VAE.load_state_dict(ckt['D_VAE'])
+            D_LR.load_state_dict(ckt['D_LR'])
+            #load optimizers
+            optimizer_G.load_state_dict(ckt['optimizer_G'])
+            optimizer_E.load_state_dict(ckt['optimizer_E'])
+            optimizer_D_VAE.load_state_dict(ckt['optimizer_D_VAE'])
+            optimizer_D_LR.load_state_dict(ckt['optimizer_D_LR'])
+            #load epoch
+            start_epoch = ckt['epoch'] + 1
+            print(f"Resuming training from epoch {start_epoch}")
+        else:
+          print("checkpoint file corrupted")
+    else:
+      print("No checkpoints detected. Starting from scratch")
+
+    #common code irrespective of checkpoint
     running_total_loss = 0
     running_loss_cvaegan_l1 = 0
     running_loss_KLD = 0
     running_loss_D_loss_cVAE = 0
     running_loss_D_loss_cLRGAN = 0
     running_loss_clrgan_l1 = 0
-    for e in range(num_epochs):
-        # start = time.time()
+    running_loss_vae_gan_loss = 0
+    running_loss_LR_GAN_loss = 0
+
+    for e in range(start_epoch, num_epochs):
         tqdm_loader = tqdm(loader, total=len(loader))
         for idx, data in enumerate(tqdm_loader):
-            # if idx >= 1000:
-            #     break
             edge_tensor, rgb_tensor = data
             edge_tensor, rgb_tensor = norm(edge_tensor).to(gpu_id), norm(rgb_tensor).to(gpu_id)
-            real_A = edge_tensor; real_B = rgb_tensor;
+            real_A = edge_tensor
+            real_B = rgb_tensor
 
-            #-------------------------------
-            #  Train Generator and Encoder
-            #------------------------------
-
-            #Encoder & Generator Loss
-
-            #zero grad
-            optimizer_G.zero_grad()
             optimizer_E.zero_grad()
+            optimizer_G.zero_grad()
+
+
+
+            # Encode real images to get latent codes (z_encoded) and their corresponding mean (mu) and log variance (logvar)
+            z_encoded, mu, logvar = encoder(real_B)
+            #print(z_encoded.shape) #[8,8]
+
+            # Generate fake images from real images and latent codes - vae generator
+            fake_B_vae = generator(real_A, z_encoded)
+
+            #next use clrgan generator, encoder
+            z_random = torch.randn_like(z_encoded)
+            fake_B_clr = generator(real_A, z_random)
+            _, mu_clr, logvar_clr2 = encoder(fake_B_clr)
+
+            #----------------------
+
+
+            #generator losses -
+            #1) cvaegan adversarial loss (pass vaegan's generator generated fake image to discriminator vae and calculate adv loss)
+            fake_D_VAE_scores = D_VAE(fake_B_vae)
+            vae_gan_loss = mse_loss(fake_D_VAE_scores, torch.ones_like(fake_D_VAE_scores))
+            # we used ones_like above as generator wants to convince disc that fake images should be predicted as 1
+
+            #2) clrgan adversarial loss - (pass clrgan generator generated fake images to disc LR and calculate adv loss)
+            fake_D_LR_scores = D_LR(fake_B_clr)
+            clr_gan_loss = mse_loss(fake_D_LR_scores, torch.ones_like(fake_D_LR_scores))
+
+            #3) KL Divergence loss
+            KLD_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+            #4) CVAEGAN Reconstruction Loss (L1 loss)
+            cvaegan_l1 = mae_loss(fake_B_vae, real_B)
+
+            #In all of components above, it doesnt make sense to freeze generator or encoder.
+            # add all losses
+            G_loss = cvaegan_l1 * lambda_pixel + KLD_loss * lambda_kl + vae_gan_loss + clr_gan_loss
+            G_loss.backward(retain_graph = True)  #Here I should use retain graph
+            #as below clrgan_l1 uses encoder, generator as was used by G_loss. So we should not free the computation graph
+
+
+            l1_clr = mae_loss(mu_clr, z_random)
+            clrgan_l1 = l1_clr * lambda_latent
+            clrgan_l1.backward()
+
+            #update optimizers
+            optimizer_E.step()
+            optimizer_G.step()
+
+            #Generator, Encoder training done
+
+            #----------------------
+            #Now we train discriminator.
+
             optimizer_D_VAE.zero_grad()
             optimizer_D_LR.zero_grad()
 
-
-            #first we pass real B to encoder in cvae-gan
-            z_encoded,mu,logvar = encoder(real_B.detach())
-            # next we pass sketch image, z to generator to get fakeB
-            fake_B_vae = generator(real_A.detach(), z_encoded.detach())
-
-
-            #we calculate l1 loss for clrgan
-            z_random = torch.randn_like(z_encoded)
-            fake_B_clr = generator(real_A.detach(),z_random.detach())
-            _, mu_clr, logvar_clr2 = encoder(fake_B_clr.detach())
-            clrgan_l1 = mae_loss(mu_clr, z_random)
-            clrgan_l1.backward()
-            optimizer_G.step()
-
-
-            #next, we calculate cvaegan L1 loss
-            cvaegan_l1  = mae_loss(fake_B_vae.detach(),real_B.detach())  #got cvaegan L1 loss
-            #next we calculate kl div loss
-            KLD = (1/2) * (torch.sum(torch.exp(logvar) + mu.pow(2) - 1 - logvar)  )  #got cvaegan KL loss
-
-            #calculate cvae gan L2 loss
-            real_D_VAE_scores = D_VAE(real_B.detach())
-            fake_D_VAE_scores = D_VAE(fake_B_vae.detach())
-            real_D_LR_scores = D_LR(real_B.detach())
-            fake_D_LR_scores = D_LR(fake_B_clr.detach())
-
-            VAE_GAN_loss =  mse_loss(fake_D_VAE_scores.detach(),torch.ones_like(fake_D_VAE_scores))
-            LR_GAN_loss = mse_loss(fake_D_LR_scores.detach(), torch.ones_like(fake_D_VAE_scores) )
-
-            loss_GE = VAE_GAN_loss + LR_GAN_loss + cvaegan_l1*lambda_pixel + KLD * lambda_kl
-
-            loss_GE.backward()
-            optimizer_E.step()
-
-
-            
-
-            #optimizing discriminators
-            
-
-            D_loss_cVAE = mse_loss(real_D_VAE_scores,torch.ones_like(real_D_VAE_scores)) + mse_loss(fake_D_VAE_scores,torch.zeros_like(fake_D_VAE_scores))
+            # Calculate discriminator loss for VAE-GAN
+            real_D_VAE_scores = D_VAE(real_B)
+            D_loss_cVAE = calculate_D_loss(fake_B_vae, D_VAE, real_B)
             D_loss_cVAE.backward()
             optimizer_D_VAE.step()
 
-
-            D_loss_cLRGAN = mse_loss(real_D_LR_scores,torch.ones_like(real_D_LR_scores)) + mse_loss(fake_D_LR_scores,torch.zeros_like(fake_D_LR_scores))
+            # Calculate discriminator loss for cLR-GAN
+            real_D_LR_scores = D_LR(real_B)
+            D_loss_cLRGAN = calculate_D_loss(fake_B_clr, D_LR, real_B)
             D_loss_cLRGAN.backward()
             optimizer_D_LR.step()
 
 
-            # """
             #add all losses
-            running_total_loss += (D_loss_cVAE +D_loss_cVAE + cvaegan_l1*lambda_pixel + KLD * lambda_kl + clrgan_l1*lambda_latent).item()
+            running_total_loss += (D_loss_cVAE +D_loss_cLRGAN + cvaegan_l1*lambda_pixel + KLD_loss * lambda_kl + clrgan_l1*lambda_latent +vae_gan_loss  +clr_gan_loss).item()
             running_loss_cvaegan_l1 += cvaegan_l1.item()
-            running_loss_KLD += KLD.item()
+            running_loss_KLD += KLD_loss.item()
             running_loss_D_loss_cVAE += D_loss_cVAE.item()
             running_loss_D_loss_cLRGAN += D_loss_cLRGAN.item()
-            running_loss_clrgan_l1 = clrgan_l1.item()
+            running_loss_clrgan_l1 += clrgan_l1.item()
+            running_loss_vae_gan_loss += vae_gan_loss.item()
+            running_loss_LR_GAN_loss += clr_gan_loss.item()
+
 
             step += 1
 
             ########## Visualization ##########
             if step % report_feq == report_feq-1:
-                print('Train Epoch: {} {:.0f}% \tTotal Loss: {:.6f} \loss_cvaegan_l1: {:.6f}\loss_KLD: {:.6f}\D_loss_cVAE: {:.6f}\D_loss_cLRGAN: {:.6f}\loss_clrgan_l1: {:.6f}'.format
+                print('Train Epoch: {} {:.0f}% \tTotal Loss: {:.6f} \loss_cvaegan_l1: {:.6f}\loss_KLD: {:.6f}\D_loss_cVAE: {:.6f}\D_loss_cLRGAN: {:.6f}\loss_clrgan_l1: {:.6f}\VAE_GAN_loss: {:.6f}\LR_GAN_loss: {:.6f}'.format
                         (e+1, 100. * idx / len(loader), running_total_loss / report_feq,
                         running_loss_cvaegan_l1/report_feq, running_loss_KLD/report_feq,
                         running_loss_D_loss_cVAE/report_feq, running_loss_D_loss_cLRGAN/report_feq,
-                        running_loss_clrgan_l1/report_feq))
+                        running_loss_clrgan_l1/report_feq,
+                         running_loss_vae_gan_loss/report_feq,
+                         running_loss_LR_GAN_loss/report_feq))
 
                 #now store losses in list for plot
                 total_loss_history.append(running_total_loss/report_feq)
@@ -208,35 +333,65 @@ def train(argpath = None):
                 loss_D_loss_cVAE_history.append(running_loss_D_loss_cVAE/report_feq)
                 loss_D_loss_cLRGAN_history.append(running_loss_D_loss_cLRGAN/report_feq)
                 loss_clrgan_l1_history.append(running_loss_clrgan_l1/report_feq)
+                loss_vae_gan_loss_history.append(running_loss_vae_gan_loss/report_feq)
+                loss_LR_GAN_loss_history.append(running_loss_LR_GAN_loss/report_feq)
 
                 #now reset once saved
-                running_loss_D_A = 0
-                running_loss_D_B = 0
-                running_loss_GAN_AB = 0
-                running_loss_GAN_BA = 0
-                running_loss_cycle = 0
                 running_total_loss = 0
-                # end = time.time()
-                # print(e, step, 'T: ', end-start)
-                # start = end
-                vis_fake_A = denorm(fake_B_vae[0].detach()).cpu().data.numpy().astype(np.uint8)
-                vis_fake_B = denorm(fake_B_clr[0].detach()).cpu().data.numpy().astype(np.uint8)
-                vis_real_B = denorm(real_B[0].detach()).cpu().data.numpy().astype(np.uint8)
-                vis_real_A = denorm(real_A[0].detach()).cpu().data.numpy().astype(np.uint8)
-                fig, axs = plt.subplots(2,2, figsize = (5,5))
+                running_loss_cvaegan_l1 = 0
+                running_loss_KLD = 0
+                running_loss_D_loss_cVAE = 0
+                running_loss_D_loss_cLRGAN = 0
+                running_loss_clrgan_l1 = 0
+                running_loss_vae_gan_loss = 0
+                running_loss_LR_GAN_loss = 0
 
-                axs[0,0].imshow(vis_real_A.transpose(1,2,0))
-                axs[0,0].set_title('real images')
-                axs[0,1].imshow(vis_fake_B.transpose(1,2,0))
-                axs[0,1].set_title('generated images')
-                axs[1,0].imshow(vis_real_B.transpose(1,2,0))
-                axs[1,1].imshow(vis_fake_A.transpose(1,2,0))
-                plt.show()
-                #Visualize generated images
-                # plt.imshow()
-                
-            
+
+                #Visualize generated images & loss
+                plot_sketch_to_multimodal(generator,real_A[0],latent_dim)
+
+                plot_loss(total_loss_history,loss_cvaegan_l1_history,
+             loss_KLD_history,loss_D_loss_cVAE_history,
+             loss_D_loss_cLRGAN_history,
+             loss_clrgan_l1_history,
+                        loss_vae_gan_loss_history,loss_LR_GAN_loss_history)
+
+            if step % save_feq == save_feq-1:
+              #Checkpointing code
+              checkpoint_filename = f'checkpoint_epoch{e}_step_{step}.pt'
+              checkpoint_path = os.path.join('/content/drive/MyDrive/classwork/cis680/project/checkpoints/', checkpoint_filename)
+              print(f"Saving checkpoint : {checkpoint_filename} at {checkpoint_path}")
+
+              checkpoint = {
+                  #store losses list
+                  "loss_total": total_loss_history,
+                  "cvaegan_l1": loss_cvaegan_l1_history,
+                  "KLD": loss_KLD_history,
+                  "D_loss_cVAE": loss_D_loss_cVAE_history,
+                  "D_loss_cLRGAN": loss_D_loss_cLRGAN_history,
+                  "clrgan_l1" : loss_clrgan_l1_history,
+                  "VAE_GAN_loss" : loss_vae_gan_loss_history,
+                  "LR_GAN_loss" : loss_LR_GAN_loss_history,
+                  #store generators, discriminators, encoders since they are getting updated in backprop
+                  "generator": generator.state_dict(),
+                  "encoder": encoder.state_dict(),
+                  "D_VAE": D_VAE.state_dict(),
+                  "D_LR": D_LR.state_dict(),
+                  #store optimizer & epoch info
+                  "optimizer_G" : optimizer_G.state_dict(),
+                  "optimizer_E": optimizer_E.state_dict(),
+                  "optimizer_D_VAE": optimizer_D_VAE.state_dict(),
+                  "optimizer_D_LR" : optimizer_D_LR.state_dict(),
+                  "epoch" : e
+
+                            }
+
+              torch.save(checkpoint, checkpoint_path)
+
+
+
     return total_loss_history,loss_cvaegan_l1_history,loss_KLD_history,loss_D_loss_cVAE_history,loss_D_loss_cLRGAN_history, loss_clrgan_l1_history
+
 
 
 
